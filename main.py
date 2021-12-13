67f1bcbc4b33436eb7879bbf48558a19d99c82fe
R4ZN1V <rezwele@gmail.com>
2021-12-13 18:28:42 +0200

    asdasdsadasd

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import rook
+import random
+
+def kaki():
+    x = random.randint(0,10)
+    x += 1
+    return x
 
 if __name__ == '__main__':
     rook.start(token='f7660f39b7396690e332d9cc4ba1f3e3b5c605dc550fbe1e54ad02d2202c0ef3', labels={"env":"dev"})
     x = 0
     while True:
-        x += 1
+        x = kaki()
         print(f"x {x}")
     print(f"number {x}")
