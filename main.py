27b8de9341051e545cfe6df6b08f9fc52cdea5ef
R4ZN1V <rezwele@gmail.com>
2021-12-13 18:25:24 +0200

    asdasdasd

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import rook
 
 if __name__ == '__main__':
     rook.start(token='f7660f39b7396690e332d9cc4ba1f3e3b5c605dc550fbe1e54ad02d2202c0ef3', labels={"env":"dev"})
+    x = 0
     while True:
-        print("!!!")
-    x = 11
+        x += 1
+        print("x {x}")
     print(f"number {x}")
